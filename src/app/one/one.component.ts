6a4f07e2ea4b88f9c43ebbade0da043f3513e5d1
benlr98 <benlr98@gmail.com>
2018-11-27 00:29:02 -0500

    Created component one and otwo

---
+++ src/app/one/one.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-one',
+    templateUrl: './one.component.html'
+})
+
+export class OneComponent {
+
+}
---
+++ src/app/otwo/otwo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-otwo',
+    templateUrl: './otwo.comonent.html'
+})
+
+export class OtwoComponent {
+
+}
